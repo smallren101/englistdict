ed395948359c2025df8f784775f2d7d2588cce23
Your Name <you@example.com>
2019-08-11 22:07:43 +0800

    first commit

---
+++ server.py
@@ -0,0 +1,50 @@
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+db_english = pymongo.MongoClient("127.0.0.1", 27017)["english"]
+
+
+def insert_mongo(collection, query):
+    args = query.get('args')
+    col = db_english[collection]
+    res = col.find_one({'args': args})
+    try:
+        res.get('_id')
+    except Exception as e:
+        col.insert(query)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        args = request.form['ci'].strip()
+        url = "http://dict.youdao.com/w/" + args + "/"
+        soup = BeautifulSoup(requests.get(url).text, features="html.parser")
+        trans = soup.find('div', {'id': 'phrsListTab'}).find_all("ul", limit=1)[0]
+        if trans:
+            trans = trans.text.strip('\n').split('\n')
+
+        pronounce = soup.find('div', {'id': 'phrsListTab'}).find("div", {"class": "baav"})
+        if pronounce:
+            pronounce = pronounce.text.strip('\n')
+
+        additional = soup.find('div', {'id': 'phrsListTab'}).find("p", {"class": "additional"})
+        if additional:
+            additional = additional.text
+
+        insert_mongo('danci', {"args": args, "trans": trans, "pronounce": pronounce, "additional": additional})
+
+        return render_template('index.html', args=args, trans=trans, pronounce=pronounce, additional=additional)
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
