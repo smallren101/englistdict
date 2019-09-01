a7c7462d976607a32f3ef127b6336f3c125704cb
Your Name <you@example.com>
2019-09-01 22:06:37 +0800

    feat(view): add page split; data export; add bootstrap

---
+++ scripts/mongoexport.py
@@ -0,0 +1,14 @@
+import json
+
+import pymongo
+
+
+mongo = pymongo.MongoClient('127.0.0.1', 27017)
+db = mongo['english']
+col = db['danci']
+
+with col.find(projection={'_id': False}, no_cursor_timeout=True) as results:
+    with open('danci.json', 'w') as f:
+        for res in results:
+            f.write(json.dumps(res))
+            f.write('\n')
--- server.py
+++ server.py
@@ -1,23 +1,25 @@
+import math
+
 import pymongo
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from flask_paginate import get_page_parameter, Pagination
 
 app = Flask(__name__)
 CORS(app)
 db_english = pymongo.MongoClient("127.0.0.1", 27017)["english"]
+col = db_english['danci']
+PER_PAGE = 13
 
 
-def insert_mongo(collection, query):
+def insert_mongo(query):
     args = query.get('args')
-    col = db_english[collection]
     res = col.find_one({'args': args})
-
     try:
         res.get('_id')
     except Exception as e:
-        print(args)
         col.insert(query)
 
 
@@ -25,29 +27,41 @@ def insert_mongo(collection, query):
 def index():
     if request.method == 'GET':
         # page = int(request.args.get('page') or 1)
-        # query = db_english['danci'].find().skip((page - 1) * 10).limit(10)
-
-        return render_template('index.html')
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        start = (page - 1) * PER_PAGE
+        pagination = Pagination(bs_version=3, page=page, total=col.count(), per_page_parameter=PER_PAGE)
+        result = list(col.find().sort('_id', 1).skip(start).limit(PER_PAGE))
+        return render_template('index.html', result=result, page=pagination)
     elif request.method == 'POST':
         args = request.form['ci'].strip()
+        if args:
+            result = [col.find_one({'args': args})]
+            if result:
+                return render_template('index.html', args=args, result=result)
+            else:
+                print(args)
+                url = "http://dict.youdao.com/w/" + args + "/"
+                soup = BeautifulSoup(requests.get(url).text, features="html.parser")
+                trans = soup.find('div', {'id': 'phrsListTab'}).find_all("ul", limit=1)[0]
+                if trans:
+                    trans = trans.text.strip('\n').split('\n')
 
-        url = "http://dict.youdao.com/w/" + args + "/"
-        soup = BeautifulSoup(requests.get(url).text, features="html.parser")
-        trans = soup.find('div', {'id': 'phrsListTab'}).find_all("ul", limit=1)[0]
-        if trans:
-            trans = trans.text.strip('\n').split('\n')
+                pronounce = soup.find('div', {'id': 'phrsListTab'}).find("div", {"class": "baav"})
+                if pronounce:
+                    pronounce = pronounce.text.strip('\n')
 
-        pronounce = soup.find('div', {'id': 'phrsListTab'}).find("div", {"class": "baav"})
-        if pronounce:
-            pronounce = pronounce.text.strip('\n')
+                additional = soup.find('div', {'id': 'phrsListTab'}).find("p", {"class": "additional"})
+                if additional:
+                    additional = additional.text
+                query = {"args": args, "trans": trans, "pronounce": pronounce, "additional": additional}
+                insert_mongo(query)
 
-        additional = soup.find('div', {'id': 'phrsListTab'}).find("p", {"class": "additional"})
-        if additional:
-            additional = additional.text
-        query = {"args": args, "trans": trans, "pronounce": pronounce, "additional": additional, "index": args[0].upper()}
-        insert_mongo('danci', query)
+                return render_template('index.html', result=query)
+        else:
+            result = list(col.find().sort('_id', 1).limit(PER_PAGE))
+            pagination = Pagination(bs_version=3, page=1, total=col.count())
+            return render_template('index.html', result=result, page=pagination)
 
-        return render_template('index.html', args=args, trans=trans, pronounce=pronounce, additional=additional)
     else:
         return render_template('index.html')
 
